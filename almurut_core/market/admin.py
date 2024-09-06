2484494755f36b9e5d14b30966dd08e01037b6bd
Nuryiman <murataliev07n@gmail.com>
2024-09-06 08:58:58 +0600

    добавлено картина в админку и категория привязана к товарам

--- almurut_core/market/admin.py
+++ almurut_core/market/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from market.models import Product, ProductGallery, ProductCategory
 
 
 class ProductGalleryInline(admin.TabularInline):
     model = ProductGallery
-    extra = 1  # Количество пустых форм для добавления новых записей
+    extra = 4  # Количество пустых форм для добавления новых записей
 
 
 @admin.register(ProductCategory)
@@ -15,9 +17,14 @@ class ProductCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'sales_percent']
+    list_display = ['name', 'price', 'sales_percent', 'image_tag',]
     search_fields = ['name', 'description']
     inlines = [ProductGalleryInline]  # Включаем галерею товаров как inline
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="100px"/>'.format(obj.preview_image.url))
+
+    image_tag.short_description = 'превью-изображения'
 
 
 @admin.register(ProductGallery)
--- almurut_core/market/models.py
+++ almurut_core/market/models.py
@@ -1,4 +1,4 @@
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
@@ -14,19 +14,25 @@ class ProductCategory(models.Model):
         verbose_name = 'Категория товара'
         verbose_name_plural = 'Категории товаров'
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     """Моделька для товаров"""
+
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=150, verbose_name='Название товара')
     description = models.TextField(verbose_name='Описание товара')
-    price = models.PositiveIntegerField(verbose_name='Цена товара')
-    preview_image = models.ImageField(verbose_name='Изображение товара')
+    price = models.PositiveIntegerField(verbose_name='Цена товара', help_text='в сомах')
     sales_percent = models.PositiveIntegerField(
         verbose_name='Скидка в процентах',
         null=True,
         validators=[MaxValueValidator(100)]
     )
-    new_expiry_date = models.DateField(verbose_name='Дата истечения срока действия')
+    preview_image = models.ImageField(verbose_name='Изображение товара', upload_to='product_previews_images/')
+
+    new_expiry_date = models.DateField(verbose_name='Дата истечения отметки Новый')
 
     class Meta:
         verbose_name = 'Товар'
@@ -36,7 +42,7 @@ class Product(models.Model):
 class ProductGallery(models.Model):
     """Моделька для галереи товаров"""
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар')
-    image = models.ImageField(verbose_name='Изображение')
+    image = models.ImageField(verbose_name='Изображение', upload_to='product_gallery')
 
     class Meta:
         verbose_name = 'Галерея товара'
